cae81885676e6860b01bbf01a24d55d1d8c09ef3
agkerans <42479476+agkerans@users.noreply.github.com>
2019-05-01 15:10:44 -0500

    working on the map class and maps, need to review arrays

--- FinalProject/ChoiceMap.java
+++ FinalProject/ChoiceMap.java
@@ -1,40 +1,33 @@
-
-
 /**
- * Write a description of class ChoiceMap here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Alexandra Kerans and Brianna Bukowski
+ * 1 May 2019
+ * Choice Map Class
+ * Can make objects that have a map that stores all the story choices
  */
+/*MAP NOTES
+ *     key value, collection value (only stores objects)
+ * Map<String,    Integer          > myMap = new Map<>(); <-- making a map
+ * myMap.put("Bob", 12); <--- adding new keys and values
+ * myMap.put("Bob", 5); <--- modifying an existing key's values
+ * myMap.get("Bob"); <--- returns the value associated with the key
+ * myMap.remove("Bob"); <--- removes the key and values associated with it
+ * 
+ * for(String key : myMap.keySet()) <--- for every key all the keys (.keySet() returns a list of the keys)
+ * {
+ *     myMap.get(key); <--- get the value associated with the key
+ *     }
+ */
+
 public class ChoiceMap
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
-
+    Map<Integer, Integer[]> choices;  
+    
     /**
-     * Default constructor for objects of class ChoiceMap
+     * user enters an array of integers (key choices)
+     * and an array of arrays [1, 2] (the two choices a user can choose at a certain choice
      */
-    public ChoiceMap()
+    public ChoiceMap(int[] keys)
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
-    }
-
 }
