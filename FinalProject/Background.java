13000c5b8f40ef6bc6d91113d7fcea42a1d2cfb1
Brianna Bukowski <bmbukowski@stu.naperville203.org>
2019-05-12 14:40:59 -0500

    Fixed mouse listener?

--- FinalProject/Background.java
+++ FinalProject/Background.java
@@ -8,8 +8,10 @@ import java.util.Random;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
-public class Background extends Applet
+public class Background extends Applet implements MouseListener
 {
    private final int APPLET_WIDTH = 1000;
    private final int APPLET_HEIGHT = 1000;
@@ -18,9 +20,7 @@ public class Background extends Applet
    private Color color;
    private int x = 0; // counter for paint method
    private Random generator = new Random();
-   
    private Rectangle choice1, choice2;
-   
    //-----------------------------------------------------------------
    //  Creates several building, a cloud, and the ground square
    //-----------------------------------------------------------------
@@ -28,11 +28,36 @@ public class Background extends Applet
  {
    choice1 = new Rectangle (100, 500, Color.blue, 350, 100); 
    choice2 = new Rectangle (550, 500, Color.green, 350, 100);
+   addMouseListener(this);
    
    setBackground (Color.gray);
    setSize (APPLET_WIDTH, APPLET_HEIGHT);
  }
  
+ public void mouseClicked(MouseEvent e) {
+     if (e.getX() >= 50 && e.getX() <= 150 && e.getY() >= 350 && e.getY() <= 500){
+         System.out.println("Left");
+     }
+     else if (e.getX() >= 500 && e.getX() <= 600 && e.getY() >= 350 && e.getY() <= 500){
+         System.out.println("Right");
+     }
+ }
+
+ public void mouseExited(MouseEvent e) {
+
+ }
+
+ public void mouseEntered(MouseEvent e) {
+
+ }
+
+ public void mousePressed(MouseEvent e) {
+ }
+
+ public void mouseReleased(MouseEvent e) {
+ 
+ }
+   
    //-----------------------------------------------------------------
    //  Paints the items on the applet.
    //-----------------------------------------------------------------
